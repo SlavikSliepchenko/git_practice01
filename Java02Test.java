ba29b59d65fb618b1c80709adca05802b46a9344
SlavikSliepchenko <viacheslavsliepchenko@gmail.com>
2025-03-17 21:29:40 +0100

    Add  from Idea

---
+++ Java02Test.java
@@ -0,0 +1,9 @@
+public class Java02Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world! Test");
+        System.out.println("From Idea");
+        
+    }
+
+}
