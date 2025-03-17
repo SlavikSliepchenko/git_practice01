30368626fb018fd2c4e879cd2742e3b97952a56c
ViacheslavSliepchenko <viacheslavsliepchenko@gmail.com>
2025-03-17 21:13:30 +0100

    Update Java02.java
    Test commit

--- Java02.java
+++ Java02.java
@@ -1,7 +1,8 @@
 public class Java02 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! Test");
+        
     }
 
 }
